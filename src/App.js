66d63995f23b41e183bc171ea71980242a6352c6
luchaninova <106754378+luchaninova@users.noreply.github.com>
2022-07-28 15:30:18 +0300

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <div className="wrapper">
+        <header className="App-header">hello</header>
+        <footer>
+          <a
+            href="https://github.com/luchaninova/react_dictionary_app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Open-source code
+          </a>{" "}
+          by Irina Luchaninova
+        </footer>
+      </div>
     </div>
   );
 }
