9dc3b3ee0491327490875f6e100b646c148b60eb
luchaninova <106754378+luchaninova@users.noreply.github.com>
2022-07-29 11:09:55 +0300

    Add search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Dictionary from "./Dictionary";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="wrapper">
         <header className="App-header">hello</header>
-        <footer>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
           <a
             href="https://github.com/luchaninova/react_dictionary_app"
             target="_blank"
